5ffb872461f8af58e5d9d49e68f3db035d19fbd3
abhirup-coregit <abhirupsamadder5@gmail.com>
2025-06-26 19:04:49 +0530

    � Cleaned up server logs for deployment

--- skillswap/skillswap-backend/server.js
+++ skillswap/skillswap-backend/server.js
@@ -103,5 +103,6 @@ mongoose
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
+
 });
